2c9aa82e6a86e6d1c18fc7914bf58545c8275309
Sureshprabusrv-cloud <sureshprabuapril@gmail.com>
2026-03-18 14:51:15 +0300

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,27 +1,14 @@
-#!/bin/bash
-
-# Update system and install Java
 sudo apt update
 sudo apt install fontconfig openjdk-17-jre -y
 
-# Add Jenkins repository key
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-# Add Jenkins repository
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-# Update repo list
 sudo apt update
-
-# Install Jenkins
 sudo apt install jenkins -y
 
-# Start & enable Jenkins service
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
-# Show Jenkins status
 systemctl status jenkins
